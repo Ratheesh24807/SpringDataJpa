ca6e9bec05724dd4e395c527ad7d5accaa97892b
Ratheesh <ratheeshagowda@gmail.com>
2023-08-22 22:12:51 +0530

    Revert "Intial commit"
    This reverts commit 380209b733adde387f6dbde3e044dfb35be9f336.

--- src/main/java/com/ratheesh/Repo/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ratheesh.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ratheesh.Entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Integer>{
-
-}
